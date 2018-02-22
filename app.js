3b883c4e5517d0c1a554589730e63330e702b6eb
Alvin Ngo <aln011@ucsd.edu>
2018-02-22 11:17:04 -0800

    added functionality

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
 app.use(express.favicon());
-app.use(express.logger('dev'));
+app.use(express.logger('dev')); 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
@@ -33,11 +33,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
-}
+} 
 
 // Add routes here
 app.get('/', index.view);
 app.get('/project/:id', project.view);
+app.get('/viewAlt', index.viewAlt);
 // Example route
 // app.get('/users', user.list);
 
